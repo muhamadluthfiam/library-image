b9a842eaee2a1ecf67897e4a50b795a188a21076
luthfi <mluthfiamrullah@gmail.com>
2023-12-06 21:19:40 +0700

    90%

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,13 @@
 import { ref, onMounted } from 'vue'
 import Navbar from './components/Navbar.vue'
 
+const userData = ref(null);
+
+onMounted(() => {
+  const storedData = localStorage.getItem('uploadedImages');
+
+  userData.value = storedData ? JSON.parse(storedData) : null;
+});
 
 </script>
 
@@ -10,28 +17,9 @@ import Navbar from './components/Navbar.vue'
     <Navbar/>
     <section class="p-5 pt-24 lg:pt-[100px]">
       <div class="grid grid-cols-2 md:grid-cols-4 gap-2 ">
-        <div class="h-15 aspect-[4/3] md:aspect-[3/4] xl:aspect-[4/3] rounded-lg md:col-start-4 md:row-start-2 md:flex md:self-center hidden">
-          <span class="hidden lg:block">
-            mluthfiamrullah
-            Github
-          </span>
+        <div class="h-15 aspect-[4/3] md:aspect-[3/4] rounded-lg overflow-hidden group relative hover:scale-95 transition-all duration-500" v-for="(image, i) in userData" :key="i">
+          <img :src="image" alt="" class="w-full h-full bg-cover bg-center absolute group-hover:scale-125 transition-all duration-500">
         </div>
-        <div class="h-15 aspect-[4/3] md:aspect-[3/4] xl:aspect-[4/3] rounded-lg md:col-start-2 md:row-start-2 md:flex md:self-end hidden">
-          <span class="md:self-end">
-            @luthfiamrullah
-          </span>
-        </div>
-        <!-- <div class="h-15 bg-red-500 aspect-[4/3] md:aspect-[3/4] xl:aspect-[4/3] rounded-lg overflow-hidden group relative hover:scale-95 transition-all duration-500">
-          <div class="w-full h-full bg-[url('/img/')] bg-cover bg-center absolute group-hover:scale-125 transition-all duration-500"></div>
-        </div> -->
-        <div class="h-15 bg-blue-500 aspect-[4/3] md:aspect-[3/4] xl:aspect-[4/3] rounded-lg"></div>
-        <div class="h-15 bg-blue-500 aspect-[4/3] md:aspect-[3/4] xl:aspect-[4/3] rounded-lg md:col-start-3"></div>
-        <div class="h-15 bg-blue-500 aspect-[4/3] md:aspect-[3/4] xl:aspect-[4/3] rounded-lg"></div>
-        <div class="h-15 bg-blue-500 aspect-[4/3] md:aspect-[3/4] xl:aspect-[4/3] rounded-lg"></div>
-        <div class="h-15 bg-blue-500 aspect-[4/3] md:aspect-[3/4] xl:aspect-[4/3] rounded-lg"></div>
-        <div class="h-15 bg-blue-500 aspect-[4/3] md:aspect-[3/4] xl:aspect-[4/3] rounded-lg"></div>
-        <div class="h-15 bg-blue-500 aspect-[4/3] md:aspect-[3/4] xl:aspect-[4/3] rounded-lg"></div>
-        <div class="h-15 bg-blue-500 aspect-[4/3] md:aspect-[3/4] xl:aspect-[4/3] rounded-lg"></div>
       </div>
     </section>
   </main>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,29 +1,118 @@
 <script setup>
-import { onMounted, ref } from 'vue'
+import Swal from 'sweetalert2'
 import axios from 'axios'
+import { computed, onMounted, ref } from 'vue'
 const isOpen = ref(false)
 
 const file = ref(null)
+const attachment = ref([])
+const isLoading = ref(false)
+
+const invalid = ref()
+
 const allowedTypeFiles = ['image/jpeg', 'image/jpg', 'image/bmp', 'image/png', 'image/gif']
 const maxFileSizeInBytes = 2 * 1024 * 1024
+
+
+const showAlert = () => {
+  Swal.fire({
+    title: 'Hello!',
+    text: 'This is an alternative to SweetAlert!',
+    icon: 'success',
+    confirmButtonText: 'OK'
+  })
+}
+
 const handleFileUpload = (e) => {
   const files = e.target.files
+  attachment.value.push(...files)
   for (let i = 0; i < files.length; i++) {
     const fileType = files[i].type;
     const fileSize = files[i].size;
 
     if (allowedTypeFiles.includes(fileType) && fileSize <= maxFileSizeInBytes) {
-      console.log(`File ${files[i].name} is valid.`);
+      const reader = new FileReader()
+      reader.onload = async (event) => {
+        attachment.value[i].preview = event.target.result;
+      };
+      isLoading.value = true     
+      setTimeout(() => {
+        isLoading.value = false     
+      }, 3000)
+      reader.readAsDataURL(files[i]);
+      // console.log(`File ${files[i].name} is valid.`);
+      // Swal.fire({
+      //   title: 'Hello!',
+      //   text: `File ${files[i].name} is valid.`,
+      //   icon: 'success',
+      //   confirmButtonText: 'OK'
+      // })
     } else {
-      console.log(`File ${files[i].name} is not valid. Check file type and size.`);
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: `File ${files[i].name} is not valid. Check file type and size.`,
+        confirmButtonText: 'OK'
+      });
+      isOpen.value = false
     }
   }
 }
 
 
-const onSubmit = () => {
-  console.log('hello')
-  axios.post("https://api.imgbb.com/1/upload?key=6bf50d5cb3ec3a5ec733f9be8f5c849f")
+const removeAttachment = (e) => {
+  console.log(attachment.value)
+  attachment.value.splice(e, 1)
+}
+
+const onSubmit = async () => {
+  try {
+    isLoading.value = true;
+
+    const base64Images = attachment.value.map((file) => {
+      return new Promise((resolve) => {
+        const reader = new FileReader();
+        reader.onload = () => resolve(reader.result);
+        reader.readAsDataURL(file);
+      });
+    });
+
+    const imageDataArray = await Promise.all(base64Images);
+
+    const imgbbApiKey = 'd7e47b86f195b0aa31933f2cabce40ca';
+    const imgbbApiUrl = `https://api.imgbb.com/1/upload?key=${imgbbApiKey}`;
+    
+    const imgUploadPromises = imageDataArray.map((base64Data) => {
+      const formData = new FormData();
+      formData.append('image', base64Data.split(',')[1]);
+
+      return axios.post(imgbbApiUrl, formData, {
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+      });
+    });
+
+    const imgbbResponses = await Promise.all(imgUploadPromises)
+
+    const imgUrls = imgbbResponses.map((response) => response.data.data.url)
+
+    localStorage.setItem('uploadedImages', JSON.stringify(imgUrls))
+    showAlert();
+
+    attachment.value = [];
+  } catch (error) {
+    console.error('Error uploading images:', error);
+    Swal.fire({
+      icon: 'error',
+      title: 'Oops...',
+      text: 'Error uploading images. Please try again.',
+      confirmButtonText: 'OK',
+    });
+  } finally {
+    isLoading.value = false;
+    isOpen.value = false;
+  }
 }
 </script>
 
@@ -47,7 +136,7 @@ const onSubmit = () => {
     <div class="bg-slate-700 opacity-50 w-full h-full fixed z-10" v-if="isOpen" @click="isOpen = !isOpen"></div>
     <!-- Modal -->
     <div v-if="isOpen" class="z-30 flex items-center justify-center h-full fixed top-0 left-0 right-0 bottom-0">
-      <div class="border border-black max-w-2xl bg-white p-8 rounded-xl relative">
+      <div class="border border-black w-full lg:max-w-2xl bg-white p-8 rounded-xl relative">
         <p>
           Upload image 
         </p>
@@ -56,42 +145,26 @@ const onSubmit = () => {
           <button @click="isOpen = !isOpen" type="button" class="border border-black hover:bg-red-400 hover:text-white focus:ring-1 focus:ring-red-200 absolute -top-4 right-0 z-30 bg-white w-8 h-8 flex items-center justify-center rounded-full">
             <span class="mb-0.5">X</span>
           </button>
-          <button type="button" @click="onSubmit" class="px-4 py-1 border rounded-lg text-white bg-green-500 mt-3 hover:shadow-md hover:bg-green-400">Add</button>
         </div>
-        <!-- <div class="mt-5 overflow-x-scroll">
-          <div class="flex space-x-4 p-4">
-            <div class="h-52 w-4/12 px-2 flex-shrink-0 relative" >
-              <button @click="isOpen = !isOpen" type="button" class="border border-black hover:bg-red-400 hover:text-white focus:ring-1 focus:ring-red-200 absolute -top-3 right-0 z-30 bg-white w-8 h-8 flex items-center justify-center rounded-full">
-                <span class="mb-0.5">X</span>
-              </button>
-              <img src="../assets/img/1.jpeg" class="w-full h-full object-cover object-center" alt="">
+        <div class="mt-5 overflow-x-scroll">
+          <div class="flex space-x-4 py-10">
+            <div v-if="isLoading">
+              Loading
             </div>
-            <div class="h-52 w-4/12 px-2 flex-shrink-0 relative" >
-              <button @click="isOpen = !isOpen" type="button" class="border border-black hover:bg-red-400 hover:text-white focus:ring-1 focus:ring-red-200 absolute -top-3 right-0 z-30 bg-white w-8 h-8 flex items-center justify-center rounded-full">
+            <div class="h-52 w-full lg:w-4/12 px-2 flex-shrink-0 relative" v-show="!isLoading" v-for="(image, i) in attachment" :key="i">
+              <button @click="removeAttachment(i)" type="button" class="border border-black hover:bg-red-400 hover:text-white focus:ring-1 focus:ring-red-200 absolute -top-3 right-0 z-30 bg-white w-8 h-8 flex items-center justify-center rounded-full">
                 <span class="mb-0.5">X</span>
               </button>
-              <img src="../assets/img/2.jpeg" class="w-full h-full object-cover object-center" alt="">
-            </div>
-            <div class="h-52 w-4/12 px-2 flex-shrink-0 relative" >
-              <button @click="isOpen = !isOpen" type="button" class="border border-black hover:bg-red-400 hover:text-white focus:ring-1 focus:ring-red-200 absolute -top-3 right-0 z-30 bg-white w-8 h-8 flex items-center justify-center rounded-full">
-                <span class="mb-0.5">X</span>
-              </button>
-              <img src="../assets/img/3.jpeg" class="w-full h-full object-cover object-center" alt="">
-            </div>
-            <div class="h-52 w-4/12 px-2 flex-shrink-0 relative" >
-              <button @click="isOpen = !isOpen" type="button" class="border border-black hover:bg-red-400 hover:text-white focus:ring-1 focus:ring-red-200 absolute -top-3 right-0 z-30 bg-white w-8 h-8 flex items-center justify-center rounded-full">
-                <span class="mb-0.5">X</span>
-              </button>
-              <img src="../assets/img/4.jpeg" class="w-full h-full object-cover object-center" alt="">
-            </div>
-            <div class="h-52 w-4/12 px-2 flex-shrink-0 relative" >
-              <button @click="isOpen = !isOpen" type="button" class="border border-black hover:bg-red-400 hover:text-white focus:ring-1 focus:ring-red-200 absolute -top-3 right-0 z-30 bg-white w-8 h-8 flex items-center justify-center rounded-full">
-                <span class="mb-0.5">X</span>
-              </button>
-              <img src="../assets/img/4.jpeg" class="w-full h-full object-cover object-center" alt="">
+              <img :src="image.preview" class="w-full h-full object-cover object-center" alt="">
+              <p v-if="image.size > maxFileSizeInBytes" class="text-sm italic font-extralight text-red-400 text-center">
+                image must be less than 2MB
+              </p>
             </div>
           </div>
-        </div> -->
+        </div>
+        <div class="flex justify-center">
+          <button type="button" @click="onSubmit" class="px-4 py-1 border rounded-lg text-white bg-green-500 mt-3 hover:shadow-md hover:bg-green-400">Submit</button>
+        </div>
       </div>
     </div>
     <!-- End Of Modal -->
